c13a5086598609a5301ca0874f9c384752a998b9
justicemuch <justinjamesqwerty@gmail.com>
2022-01-25 22:32:38 +0200

    add changes , learning go with a test project day 3,4,5

---
+++ hello-world/pkg/handlers/handlers.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"hello_world/pkg/models"
+	"hello_world/pkg/config"
+	"hello_world/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+//Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "This is the home page")
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+//ABout is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	stringMap := make(map[string]string)
+	stringMap["test"] = "world"
+	// sum := addValues(2, 2)
+	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
+// func Divide(w http.ResponseWriter, r *http.Request){
+// 	f, err := divideValues(100.0, 0.0)
+// 	if err != nil{
+// 		fmt.Fprintf(w, err.Error())
+// 		return
+// 	}
+
+// 	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is  %f", 100.0 ,0.0 ,f))
+// }
+
+// func divideValues(x, y float32) (float32, error){
+// 	var err error
+// 	if y == 0{
+// 		err = errors.New("cannot divide by zero")
+// 	}
+
+// 	result := x/y
+
+// 	return result, err
+
+// }
